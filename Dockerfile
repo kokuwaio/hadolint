1dae036f6ff2e6e65a684a27e90d233bedce91dd
Stephan Schnabel <stephan.schnabel@grayc.de>
2025-12-17 10:38:56 +0100

    feat(docker): switch from bash to busybox

--- Dockerfile
+++ Dockerfile
@@ -1,18 +1,20 @@
-# ignore pipefail because
-# bash is non-default location https://github.com/tianon/docker-bash/issues/29
-# hadolint only uses default locations https://github.com/hadolint/hadolint/issues/977
-# hadolint global ignore=DL4006
+# busybox contains wget that can be used todownload files and we could reduce the dependencies to one image,
+# but wget does not support tls verification (https://github.com/docker-library/busybox/issues/80)
+# and wget fails on arch arm64 (https://github.com/docker-library/busybox/issues/162#issuecomment-1773905855)
 
-FROM docker.io/library/bash:5.3.9@sha256:df9684c6e2a1fdc5f99ce7472de257d3d26d052547481fb00341d0d0df291e8d
-SHELL ["/usr/local/bin/bash", "-u", "-e", "-o", "pipefail", "-c"]
-
+FROM docker.io/curlimages/curl:8.17.0@sha256:935d9100e9ba842cdb060de42472c7ca90cfe9a7c96e4dacb55e79e560b3ff40 AS build
+SHELL ["/bin/ash", "-u", "-e", "-o", "pipefail", "-c"]
 ARG TARGETARCH
-RUN [[ $TARGETARCH == amd64 ]] && export ARCH=x86_64; \
-	[[ $TARGETARCH == arm64 ]] && export ARCH=arm64; \
-	[[ -z ${ARCH:-} ]] && echo "Unknown arch: $TARGETARCH" && exit 1; \
-	wget -q "https://github.com/hadolint/hadolint/releases/download/v2.14.0/hadolint-Linux-$ARCH" --output-document=/usr/local/bin/hadolint && \
-	chmod 555 /usr/local/bin/hadolint
+RUN [ "$TARGETARCH" = amd64 ] && export ARCH=x86_64; \
+	[ "$TARGETARCH" = arm64 ] && export ARCH=arm64; \
+	[ -z "${ARCH:-}" ] && echo "Unknown arch: $TARGETARCH" && exit 1; \
+	curl --fail --silent --location --remote-name-all "https://github.com/hadolint/hadolint/releases/download/v2.14.0/{hadolint-linux-$ARCH,hadolint-linux-$ARCH.sha256}" && \
+	sha256sum -c -s "hadolint-linux-$ARCH.sha256" && rm "hadolint-linux-$ARCH.sha256" && \
+	mv "hadolint-linux-$ARCH" /tmp/hadolint && chmod +x /tmp/hadolint && \
+	/tmp/hadolint --version
 
-COPY --chmod=555 entrypoint.sh /usr/local/bin/entrypoint.sh
-ENTRYPOINT ["/usr/local/bin/entrypoint.sh"]
+FROM docker.io/library/busybox:1.37.0-uclibc@sha256:48a4462d62e106f6ece30479d2b198714d33cab795b6b1ad365b3f2c04ad360a
+COPY --chmod=555 --chown=0:0 --from=build /tmp/hadolint /usr/bin/hadolint
+COPY --chmod=555 --chown=0:0 entrypoint.sh /usr/bin/entrypoint.sh
+ENTRYPOINT ["/usr/bin/entrypoint.sh"]
 USER 1000:1000
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/sh
 set -eu;
 
 ##
@@ -6,58 +6,51 @@ set -eu;
 ##
 
 COMMAND="hadolint"
-if [[ -n "${PLUGIN_FORMAT:-}" ]]; then
-	COMMAND+=" --format=$PLUGIN_FORMAT"
+if [ -n "${PLUGIN_FORMAT:-}" ]; then
+	COMMAND="$COMMAND --format=$PLUGIN_FORMAT"
 fi
-if [[ -n "${PLUGIN_FAILURE_THRESHOLD:-}" ]]; then
-	COMMAND+=" --failure-threshold=$PLUGIN_FAILURE_THRESHOLD"
+if [ -n "${PLUGIN_FAILURE_THRESHOLD:-}" ]; then
+	COMMAND="$COMMAND --failure-threshold=$PLUGIN_FAILURE_THRESHOLD"
 else
-	COMMAND+=" --failure-threshold=style"
+	COMMAND="$COMMAND --failure-threshold=style"
 fi
-if [[ "${PLUGIN_STRICT_LABELS:-}" == "true" ]]; then
-	COMMAND+=" --strict-labels"
+if [ "${PLUGIN_STRICT_LABELS:-}" = "true" ]; then
+	COMMAND="$COMMAND --strict-labels"
 fi
-if [[ "${PLUGIN_VERBOSE:-}" == "true" ]]; then
-	COMMAND+=" --verbose"
+if [ "${PLUGIN_VERBOSE:-}" = "true" ]; then
+	COMMAND="$COMMAND --verbose"
 fi
-if [[ "${PLUGIN_DISABLE_IGNORE_PRAGMA:-}" == "true" ]]; then
-	COMMAND+=" --disable-ignore-pragmal"
+if [ "${PLUGIN_DISABLE_IGNORE_PRAGMA:-}" = "true" ]; then
+	COMMAND="$COMMAND --disable-ignore-pragmal"
 fi
-if [[ "${PLUGIN_NO_COLOR:-}" == "true" ]]; then
-	COMMAND+=" --no-color"
+if [ "${PLUGIN_NO_COLOR:-}" = "true" ]; then
+	COMMAND="$COMMAND --no-color"
 fi
-if [[ "${PLUGIN_NO_FAIL:-}" == "true" ]]; then
-	COMMAND+=" --no-fail"
+if [ "${PLUGIN_NO_FAIL:-}" = "true" ]; then
+	COMMAND="$COMMAND --no-fail"
 fi
 
 # custom args, e.g. docker run --rm --volume=$(pwd):$(pwd) --workdir=$(pwd) --env=CI=test kokuwaio/hadolint --format=json
-if [[ -n "${1:-}" ]]; then
-	COMMAND+=" $*"
+if [ -n "${1:-}" ]; then
+	COMMAND="$COMMAND $*"
 fi
 
 ##
 ## collect files
 ##
 
-FILES=$(find "$(pwd)" -type f \
-	\( -name 'Dockerfile' \
-	-o -name 'Containerfile' \) \
-	-not -path '*/node_modules/*')
-if [[ ! "$FILES" ]]; then
+FILES=$(find "$PWD" -type f \( -name 'Dockerfile' -o -name 'Containerfile' \) -not -path '*/node_modules/*')
+if [ ! "$FILES" ]; then
 	echo "No files found!"
 	exit 1
 fi
 for FILE in ${FILES}; do
-	COMMAND+=" \n $FILE"
+	COMMAND="$COMMAND $FILE"
 done
 
 ##
 ## evecute command
 ##
 
-if [[ -n "${CI:-}" ]]; then
-	echo "$COMMAND"
-else
-	echo "${COMMAND//\\n/}"
-fi
-eval "${COMMAND//\\n/}"
+echo "$COMMAND"
+eval "$COMMAND"
